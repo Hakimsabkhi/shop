4a067d8f4cd0f223e3d4453bef2ebea811f25a86
Hakimsabkhi <62035010+Hakimsabkhi@users.noreply.github.com>
2024-02-26 22:02:58 +0100

    2/26/24

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import { Container } from "react-bootstrap";
-import Header from "./componnent/Header";
-import Footer from "./componnent/Footer";
-
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <Header/>
-        <main>
-          <Container>
-            <h1>welcome</h1>
-          </Container>
-        </main>
-        <Footer />
-      
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
