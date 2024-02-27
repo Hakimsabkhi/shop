3e4e797abbb45ab0ec380323f8ddfc082d46d01f
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-02-27 12:32:45 +0100

    2/27/24

---
+++ frontend/src/slices/productApiSlice.js
@@ -0,0 +1,14 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productsSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({ url: PRODUCTS_URL }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+
+export const { useGetProductsQuery} = productsSlice;
