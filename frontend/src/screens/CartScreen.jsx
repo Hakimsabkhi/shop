8b49820c1da1ebe43a54797fa215e0affe9b265e
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-02-27 18:03:14 +0100

    2/27/24

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { FaTrash } from "react-icons/fa";
 import {
   Row,
   Col,
@@ -16,7 +17,71 @@ const CartScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  return <div>CartScreen</div>;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: "20px" }}> Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            {" "}
+            Your cart is emty <Link>Go Back</Link>{" "}
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>
+                    <strong>${item.price}</strong>
+                  </Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) => {}}
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button type = 'button' variant = 'light'><FaTrash/></Button>
+                  </Col>
+                  
+                </Row>
+              </ListGroup.Item>
+            ))}
+            ;
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc +item.qty, 0)})
+                items
+              </h2>
+              ${ cartItems.reduce((acc, item) => acc +item.qty* item.price, 0).toFixed(2)}
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
