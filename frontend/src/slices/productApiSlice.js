b2b19db2d368f7377181e4d0c9050357094a1e2a
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-02-27 17:28:55 +0100

    2/27/24

--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -1,14 +1,18 @@
 import { PRODUCTS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
+
 export const productsSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({ url: PRODUCTS_URL }),
       keepUnusedDataFor: 5,
     }),
+    getProductDetails: builder.query({
+      query: (productId) => ({ url: `${PRODUCTS_URL}/${productId} ` }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-
-export const { useGetProductsQuery} = productsSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsSlice;
