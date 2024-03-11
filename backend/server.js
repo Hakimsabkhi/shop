8a77b899c938c689d09fd7a7a2e56e0abb7b85a6
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-03-11 08:43:34 +0100

    3/11/24

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 // cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -34,6 +30,18 @@ app.get("api/config/paypal", (req, res) =>
     clientId: process.env.PAYPAL_CLIENT_ID,
   })
 );
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
